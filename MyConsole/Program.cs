e7c45dfbbc570052d30cf0dcd232c6f309a4ae02
Dan Fernandez <danielfe@DanielfeMac.local>
2017-10-24 20:45:14 -0300

    update to read from env var

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -9,9 +9,15 @@ namespace MyConsole
         {
             Console.WriteLine("Hello World!");
             
+            string youtubeUrl = @"https://www.youtube.com/watch?v=Vhh_GeBPOhs";
+            if (! string.IsNullOrEmpty(Environment.GetEnvironmentVariable("url")))
+            {
+                youtubeUrl = Environment.GetEnvironmentVariable("url");
+            }
+
             //take input from args
             Process p = new Process();
-            var start = new ProcessStartInfo("youtube-dl", "--write-info-json https://www.youtube.com/watch?v=Vhh_GeBPOhs");
+            var start = new ProcessStartInfo("youtube-dl", "--write-info-json "+ youtubeUrl);
             p.StartInfo = start;
 
             p.Start();
--- README.md
+++ README.md
@@ -13,4 +13,5 @@ This project requires *apt-get* to install *youtube-dl* and therefore cannot run
 * Run: "dotnet publish -o $(pwd)/app" to publish a Framework-Dependent version to the "/app" directory
 * Run: "docker build -t myconsole:dev ." to build the image. The first build will take some time as apt-get and youtube-dl have a number of dependencies/packages. Future builds will be fast.
 * Run: "docker run myconsole:dev"
+* Run with environment variable & shared drive to read files out: "docker run -v $(pwd)/app:/app -e url=https://www.youtube.com/watch?v=Y7SWYEW_K_I myconsole:dev"
 * View Logs: Use "docker ps -a" to get the container id for myconsole:dev (ex: 1a7867a6615f), then "docker logs 1a7867a6615f"
