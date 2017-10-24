fd209d421494e2e4f70d0098b66b70bf1d93e99c
Dan Fernandez <danielfe@DanielfeMac.local>
2017-10-24 19:37:10 -0300

    dotnet core console

---
+++ MyConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            //take input from args
+            Process p = new Process();
+            var start = new ProcessStartInfo("youtube-dl", "--write-info-json https://www.youtube.com/watch?v=Vhh_GeBPOhs");
+            p.StartInfo = start;
+
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+}
